8ea943aca6f48c7d798dddd52db3fd28a18ccd2a
joeybloggs <Dean.Karn@gmail.com>
2016-05-01 09:25:19 -0400

    update to 100% test coverage in main package

--- pkg_test.go
+++ pkg_test.go
@@ -54,7 +54,7 @@ func (th *testHandler) handleLogEntry(entries <-chan *Entry) {
 	}
 }
 
-func TestConsoleLogger(t *testing.T) {
+func TestConsoleLogger2(t *testing.T) {
 
 	buff := new(bytes.Buffer)
 
@@ -237,22 +237,217 @@ func TestConsoleLogger(t *testing.T) {
 	buff.Reset()
 }
 
-func TestLevel(t *testing.T) {
-	l := Level(9999)
-	Equal(t, l.String(), "Unknow Level")
+func TestConsoleLoggerCaller2(t *testing.T) {
 
-	Equal(t, DebugLevel.String(), "DEBUG")
-	Equal(t, TraceLevel.String(), "TRACE")
-	Equal(t, InfoLevel.String(), "INFO")
-	Equal(t, NoticeLevel.String(), "NOTICE")
-	Equal(t, WarnLevel.String(), "WARN")
-	Equal(t, ErrorLevel.String(), "ERROR")
-	Equal(t, PanicLevel.String(), "PANIC")
-	Equal(t, AlertLevel.String(), "ALERT")
-	Equal(t, FatalLevel.String(), "FATAL")
+	buff := new(bytes.Buffer)
+
+	SetCallerInfo(true)
+
+	th := &testHandler{
+		writer: buff,
+	}
+
+	RegisterHandler(th, AllLevels...)
+
+	Equal(t, GetCallerInfo(), true)
+
+	Debug("debug")
+	Equal(t, buff.String(), "debug")
+	buff.Reset()
+
+	Debugf("%s", "debugf")
+	Equal(t, buff.String(), "debugf")
+	buff.Reset()
+
+	Info("info")
+	Equal(t, buff.String(), "info")
+	buff.Reset()
+
+	Infof("%s", "infof")
+	Equal(t, buff.String(), "infof")
+	buff.Reset()
+
+	Notice("notice")
+	Equal(t, buff.String(), "notice")
+	buff.Reset()
+
+	Noticef("%s", "noticef")
+	Equal(t, buff.String(), "noticef")
+	buff.Reset()
+
+	Warn("warn")
+	Equal(t, buff.String(), "warn")
+	buff.Reset()
+
+	Warnf("%s", "warnf")
+	Equal(t, buff.String(), "warnf")
+	buff.Reset()
+
+	Error("error")
+	Equal(t, buff.String(), "error")
+	buff.Reset()
+
+	Errorf("%s", "errorf")
+	Equal(t, buff.String(), "errorf")
+	buff.Reset()
+
+	Alert("alert")
+	Equal(t, buff.String(), "alert")
+	buff.Reset()
+
+	Alertf("%s", "alertf")
+	Equal(t, buff.String(), "alertf")
+	buff.Reset()
+
+	Print("print")
+	Equal(t, buff.String(), "print")
+	buff.Reset()
+
+	Printf("%s", "printf")
+	Equal(t, buff.String(), "printf")
+	buff.Reset()
+
+	Println("println")
+	Equal(t, buff.String(), "println")
+	buff.Reset()
+
+	PanicMatches(t, func() { Panic("panic") }, "panic")
+	Equal(t, buff.String(), "panic")
+	buff.Reset()
+
+	PanicMatches(t, func() { Panicf("%s", "panicf") }, "panicf")
+	Equal(t, buff.String(), "panicf")
+	buff.Reset()
+
+	PanicMatches(t, func() { Panicln("panicln") }, "panicln")
+	Equal(t, buff.String(), "panicln")
+	buff.Reset()
+
+	// WithFields
+	WithFields(F("key", "value")).Info("info")
+	Equal(t, buff.String(), "info key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Infof("%s", "infof")
+	Equal(t, buff.String(), "infof key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Notice("notice")
+	Equal(t, buff.String(), "notice key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Noticef("%s", "noticef")
+	Equal(t, buff.String(), "noticef key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Debug("debug")
+	Equal(t, buff.String(), "debug key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Debugf("%s", "debugf")
+	Equal(t, buff.String(), "debugf key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Warn("warn")
+	Equal(t, buff.String(), "warn key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Warnf("%s", "warnf")
+	Equal(t, buff.String(), "warnf key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Error("error")
+	Equal(t, buff.String(), "error key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Errorf("%s", "errorf")
+	Equal(t, buff.String(), "errorf key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Alert("alert")
+	Equal(t, buff.String(), "alert key=value")
+	buff.Reset()
+
+	WithFields(F("key", "value")).Alertf("%s", "alertf")
+	Equal(t, buff.String(), "alertf key=value")
+	buff.Reset()
+
+	PanicMatches(t, func() { WithFields(F("key", "value")).Panicf("%s", "panicf") }, "panicf key=value")
+	Equal(t, buff.String(), "panicf key=value")
+	buff.Reset()
+
+	PanicMatches(t, func() { WithFields(F("key", "value")).Panic("panic") }, "panic key=value")
+	Equal(t, buff.String(), "panic key=value")
+	buff.Reset()
+
+	func() {
+		defer Trace("trace").End()
+	}()
+
+	// TODO: finish up regex
+	MatchRegex(t, buff.String(), "^trace\\s+\\.*")
+	buff.Reset()
+
+	func() {
+		defer Tracef("tracef").End()
+	}()
+
+	// TODO: finish up regex
+	MatchRegex(t, buff.String(), "^tracef\\s+\\.*")
+	buff.Reset()
+
+	func() {
+		defer WithFields(F("key", "value")).Trace("trace").End()
+	}()
+
+	// TODO: finish up regex
+	MatchRegex(t, buff.String(), "^trace\\s+\\.*")
+	buff.Reset()
+
+	func() {
+		defer WithFields(F("key", "value")).Tracef("tracef").End()
+	}()
+
+	// TODO: finish up regex
+	MatchRegex(t, buff.String(), "^tracef\\s+\\.*")
+	buff.Reset()
+
+	// Test Custom Entry ( most common case is Unmarshalled from JSON when using centralized logging)
+	entry := new(Entry)
+	entry.ApplicationID = "APP"
+	entry.Level = InfoLevel
+	entry.Timestamp = time.Now().UTC()
+	entry.Message = "Test Message"
+	entry.Fields = make([]Field, 0)
+	Logger.HandleEntry(entry)
+	Equal(t, buff.String(), "Test Message")
+	buff.Reset()
 }
 
-func TestSettings(t *testing.T) {
+func TestFatal2(t *testing.T) {
+	var i int
+
+	exitFunc = func(code int) {
+		i = code
+	}
+
+	Fatal("fatal")
+	Equal(t, i, 1)
+
+	Fatalf("fatalf")
+	Equal(t, i, 1)
+
+	Fatalln("fatalln")
+	Equal(t, i, 1)
+
+	WithFields(F("key", "value")).Fatal("fatal")
+	Equal(t, i, 1)
+
+	WithFields(F("key", "value")).Fatalf("fatalf")
+	Equal(t, i, 1)
+}
+
+func TestSettings2(t *testing.T) {
 	RegisterDurationFunc(func(d time.Duration) string {
 		return fmt.Sprintf("%gs", d.Seconds())
 	})
@@ -260,7 +455,7 @@ func TestSettings(t *testing.T) {
 	SetTimeFormat(time.RFC1123)
 }
 
-func TestEntry(t *testing.T) {
+func TestEntry2(t *testing.T) {
 
 	SetApplicationID("app-log")
 
